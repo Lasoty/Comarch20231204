559e41855167af0259a160cc9e716834a8c572fb
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-12-06 12:35:38 +0100

    Dodawanie książki [Błąd]

---
+++ Bibliotekarz/Bibliotekarz.Client/Bibliotekarz.Client/Pages/AddBookPage.razor.cs
@@ -0,0 +1,39 @@
+﻿using Bibliotekarz.Client.Services;
+using Bibliotekarz.Shared.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace Bibliotekarz.Client.Pages;
+
+public partial class AddBookPage
+{
+    public BookDto Vm { get; set; } = new()
+    {
+        Borrower = new CustomerDto()
+    };
+
+    [Inject]
+    public IBookService BookService { get; set; }
+
+    public NavigationManager Navigation { get; set; }
+
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+
+    private async void OnSaveClicked()
+    {
+        if (!Vm.IsBorrowed)
+        {
+            Vm.Borrower = null;
+        }
+
+        bool isSaved = await BookService.AddBook(Vm);
+
+        if (isSaved)
+        {
+            Navigation.NavigateTo("/");
+        }
+    }
+
+}
